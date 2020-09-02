76830aaf28cbb855fc6556524980db7f873c166d
Nico Stranquist <nstranquist@gmail.com>
2020-09-02 14:40:01 -0500

    add environment variables

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import { testEnv } from '../settings';
 
 const indexRouter = express.Router();
 
-indexRouter.get('/', (req, res) => res.status(200).json({ message: 'Welcome to Express API template' }));
+indexRouter.get('/', (req, res) => res.status(200).json({ message: testEnv }));
 
 export default indexRouter;
